7598adb11e0011570f41269dd0f09cad82e6fea8
Eric Heidel <ericleeheidel@yahoo.com>
2024-06-26 16:27:22 -0500

    get all conditions working

---
+++ Controllers/ConditionController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using OmgShoes.Data;
+
+namespace OmgShoes.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConditionController : ControllerBase
+{
+    private OmgShoesDbContext _dbContext;
+    public ConditionController(OmgShoesDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    // [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext
+            .Conditions
+            .ToList()
+        );
+    }
+}
