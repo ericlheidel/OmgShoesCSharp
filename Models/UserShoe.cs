85dc9dd9c87cfd0972d4646bdf960193e00337f0
Eric Heidel <ericleeheidel@yahoo.com>
2024-06-27 13:58:09 -0500

    add shoe to users collection working

--- Models/UserShoe.cs
+++ Models/UserShoe.cs
@@ -6,7 +6,6 @@ public class UserShoe
     public int UserProfileId { get; set; }
     public int ShoeId { get; set; }
     public Shoe? Shoe { get; set; }
-    public string? Style { get; set; }
     public string? ShoeSize { get; set; }
     public int ConditionId { get; set; }
     public Condition? Condition { get; set; }
