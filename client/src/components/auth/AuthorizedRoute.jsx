719b24a21fc07c889d888202d83fa5a2e767ee1b
Eric Heidel <ericleeheidel@yahoo.com>
2024-06-30 17:24:56 -0500

    navigation to /shoes complete, component is next

--- client/src/components/auth/AuthorizedRoute.jsx
+++ client/src/components/auth/AuthorizedRoute.jsx
@@ -17,5 +17,5 @@ export const AuthorizedRoute = ({ children, loggedInUser, roles, all }) => {
     }
   }
 
-  return authed ? children : <Navigate to="/login" />
+  return authed ? children : <Navigate to="/" />
 }
